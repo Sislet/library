06fd4d46799587aa844d3be397e93cd7782da6ea
Sislet <1343188487@qq.com>
2021-03-23 01:24:59 +0800

    这是-个rebase master onto dev的过程中的解决冲突的提交

--- a.php
+++ a.php
@@ -1,4 +1,12 @@
 
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: kk
+ * @Date: 2021-03-22 00:33:56
+ * @LastEditors: kk
+ * @LastEditTime: 2021-03-23 01:20:39
+ */
 '开始练习第一次远程提交'远程端我在这里修改了
 <<<<<<< HEAD
 我本地也改，看谁覆盖谁
@@ -6,3 +14,4 @@
 远程端第二次修改，验证fetch
 >>>>>>> origin
 在master分支修改a文件，哈哈我也这一行修改了
+我在dev分支修改了a.php文件
